1777d67de98b6b11237c769d3f8e4bac4815ec25
João Almeida <overplayed1@gmail.com>
2023-09-20 17:15:38 -0300

    1° parte do curso

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,4 +1,25 @@
-﻿string nome;
-string artista;
-int duracao;
-bool disponivel;
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) {
+            Console.WriteLine("Disponível no plano.");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus");
+        }
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Freak on a Leash";
+musica1.artista = "KoRn";
+musica1.duracao = 280;
+musica1.disponivel = true;
+
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
